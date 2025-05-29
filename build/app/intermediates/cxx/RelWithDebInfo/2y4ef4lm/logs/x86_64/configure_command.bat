48a630ac0a383abbf50ff9e79466384358d6c078
ummpasta <100769797+tiankunr@users.noreply.github.com>
2025-05-29 13:07:16 +0800

    Add build directory with compiled files

---
+++ build/app/intermediates/cxx/RelWithDebInfo/2y4ef4lm/logs/x86_64/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\tyisc\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\tyisc\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\tyisc\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\tyisc\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\tyisc\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\tyisc\\StudioProjects\\crosswordfromonline\\build\\app\\intermediates\\cxx\\RelWithDebInfo\\2y4ef4lm\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\tyisc\\StudioProjects\\crosswordfromonline\\build\\app\\intermediates\\cxx\\RelWithDebInfo\\2y4ef4lm\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\tyisc\\StudioProjects\\crosswordfromonline\\android\\app\\.cxx\\RelWithDebInfo\\2y4ef4lm\\x86_64" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
