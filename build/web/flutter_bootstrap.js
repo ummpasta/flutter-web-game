f132abd83aff34318e5e46d72bcc63794a08837a
ummpasta <100769797+tiankunr@users.noreply.github.com>
2025-06-04 15:19:37 +0800

    Update web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2950899677"
+    serviceWorkerVersion: "982277441"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -231,7 +231,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "893e6ae5afbab62d7fb15c39cd4147d3
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a1b0494c8730e966d64c7593125b771a",
+"flutter_bootstrap.js": "af38a2a0d560af824f2a3c3c9c76ce77",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
